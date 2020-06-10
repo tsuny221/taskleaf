cd44d03fb0225659e63e91709eb73c439036e61d
tsuny221 <kingoftsuny@gmail.com>
2020-06-10 01:59:00 +0000

    [Start]5-6

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,55 @@
+class TasksController < ApplicationController
+   before_action :set_task, only: [:show, :edit, :update, :destroy]
+  def index
+    @q = current_user.tasks.ransack(params[:q])
+    @tasks = @q.result(distinct: true)
+  end
+
+  def show
+    @task = current_user.tasks.find(params[:id])
+  end
+  def new
+  	@task = Task.new
+  end
+  def confirm_new
+    @task = current_user.tasks.new(task_params)
+    render :new unless @task.valid?
+  end
+  def create
+  	@task = current_user.tasks.new(task_params)
+    if params[:back].present?
+      render :new
+      return
+    end
+  	if @task.save
+      TaskMailer.creation_email(@task).deliver_now
+  	   redirect_to tasks_path, notice: "タスク「#{@task.name}」を登録しました。"
+  else
+    render :new
+  end
+  end
+
+  def edit
+    @task = current_user.tasks.find(params[:id])
+  end
+  def update
+    @task = current_user.tasks.find(params[:id])
+   if @task.update(task_params)
+      redirect_to tasks_path, notice: "タスク「#{@task.name}」を更新しました。"
+    else
+      render :edit
+  end
+  end
+  def destroy
+    task = current_user.tasks.find(params[:id])
+    task.destroy
+    redirect_to tasks_path, notice: "タスク「#{task.name}」を削除しました。"
+  end
+  private
+  def task_params
+  	params.require(:task).permit(:name, :description)
+  end
+  def set_task
+    @task = current_user.tasks.find(params[:id])
+  end
+end
---
+++ spec/mailers/taskmailer_spec.rb
@@ -0,0 +1,5 @@
+require "rails_helper"
+
+RSpec.describe TaskmailerMailer, type: :mailer do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
